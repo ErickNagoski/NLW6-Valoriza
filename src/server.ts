698295126fdd03162c5a093c2079422c8bd25f62
Erick Nagoski <ericknagoski@gmail.com>
2021-06-21 19:55:29 -0300

    Aula 1

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.get("/test", (request,response)=>{
+    //request => entrando
+    //response => saida
+    return response.send("Olá NLW")
+});
+
+app.post("/test-post", (request, response)=>{
+    return response.send("Olá NLW método POST"); 
+})
+
+
+// http://localhost:3000
+app.listen(3000, ()=> console.log("server is running"));
+    
