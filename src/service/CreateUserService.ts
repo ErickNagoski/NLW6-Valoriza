17b10342062e1f1335b75938fad2c1e11fcc0ecb
Erick Nagoski <ericknagoski@gmail.com>
2021-06-23 20:25:38 -0300

    create user

---
+++ src/service/CreateUserService.ts
@@ -0,0 +1,40 @@
+import {getCustomRepository} from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories"
+
+interface IUserRequesty{
+    name:string;
+    email:string;
+    admin?:boolean;
+}
+
+class CreateUserService{
+    async execute({name,email,admin}){
+        const usersRepository = getCustomRepository (UsersRepositories);
+
+        if(!email){
+            throw new Error("Email incorrect")
+        }
+
+        const userAlredyExists = await usersRepository.findOne({
+            email
+        });
+
+        if(userAlredyExists){
+            throw new Error("User alredy exists")
+        }
+
+        const user = usersRepository.create({
+            name,
+            email,
+            admin
+        })
+
+        await usersRepository.save(user);
+
+        return user;
+
+    }
+
+}
+
+export {CreateUserService}
