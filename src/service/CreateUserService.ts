e087e37121b2861b6f606bd3204c4ae0691988a5
Erick Nagoski <ericknagoski@gmail.com>
2021-06-26 21:38:39 -0300

    Create Compliments

--- src/service/CreateUserService.ts
+++ src/service/CreateUserService.ts
@@ -10,7 +10,7 @@ interface IUserRequesty{
 }
 
 class CreateUserService{
-    async execute({name,email,admin,password}){
+    async execute({name,email,admin = false,password}){
         const usersRepository = getCustomRepository (UsersRepositories);
 
         if(!email){
